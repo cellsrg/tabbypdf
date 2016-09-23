1ece1dd534241fedcc8e21ace51cc8edbd20b140
aaltaev <andalt92@gmail.com>
2016-09-23 15:02:13 +0800

    Implemented TextBlock logic

--- src/main/java/ru/icc/cells/common/TextBlock.java
+++ src/main/java/ru/icc/cells/common/TextBlock.java
@@ -1,6 +1,15 @@
 package ru.icc.cells.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by sunveil on 27/06/16.
  */
-public class TextBlock {}
+public class TextBlock extends Rectangle {
+    private List<TextChunk> chunks = new ArrayList<>();
+
+    public List<TextChunk> getChunks() {
+        return chunks;
+    }
+}
