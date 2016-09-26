103e2545682770ecd46e72b0379ebbbbc2869b63
aaltaev <andalt92@gmail.com>
2016-09-26 11:24:14 +0800

    Implemented TextBlock logic
    Implemented TextBlock logic such as adding of text chunk or other text block, etc.

--- src/main/java/ru/icc/cells/common/TextBlock.java
+++ src/main/java/ru/icc/cells/common/TextBlock.java
@@ -1,15 +1,69 @@
 package ru.icc.cells.common;
 
+import com.itextpdf.text.pdf.parser.Vector;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by sunveil on 27/06/16.
  */
-public class TextBlock extends Rectangle {
+public class TextBlock extends Rectangle implements Orderable {
+
+    private int order;
+
+    public TextBlock() {
+        setStartLocation(new Vector(0, 0, 0));
+        setEndLocation(new Vector(0, 0, 0));
+        setRightTopPoint(new Vector(0, 0, 0));
+    }
+
     private List<TextChunk> chunks = new ArrayList<>();
 
     public List<TextChunk> getChunks() {
         return chunks;
+    }
+
+    public void add(TextChunk textChunk) {
+        join(textChunk);
+        chunks.add(textChunk);
+    }
+
+    public void add(TextBlock textBlock) {
+        join(textBlock);
+        chunks.addAll(textBlock.chunks);
+    }
+
+    @Override
+    protected <T extends Rectangle> void join(T other) {
+        if (isEmpty()) {
+            setStartLocation(other.startLocation);
+            setEndLocation(other.endLocation);
+            setRightTopPoint(other.rightTopPoint);
+        } else {
+            super.join(other);
+        }
+    }
+
+    public boolean isEmpty() {
+        return chunks.isEmpty();
+    }
+
+    @Override
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    @Override
+    public int getOrder() {
+        return order;
+    }
+
+    public String getText() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (TextChunk chunk : chunks) {
+            stringBuilder.append(chunk.getText());
+        }
+        return stringBuilder.toString();
     }
 }
