ddc1f4064cc68f7e5e1aadf3aff127f52afbd598
aaltaev <andalt92@gmail.com>
2016-09-26 11:22:33 +0800

    Implemented rectangle logic
    Implemented rectangle logic such as rectangle joining and contains method

--- src/main/java/ru/icc/cells/common/Rectangle.java
+++ src/main/java/ru/icc/cells/common/Rectangle.java
@@ -21,4 +21,36 @@ public class Rectangle {
     public Vector getRightTopPoint() {
         return rightTopPoint;
     }
+
+    public void setStartLocation(Vector startLocation) {
+        this.startLocation = startLocation;
+    }
+
+    public void setEndLocation(Vector endLocation) {
+        this.endLocation = endLocation;
+    }
+
+    public void setRightTopPoint(Vector rightTopPoint) {
+        this.rightTopPoint = rightTopPoint;
+    }
+
+    protected <T extends Rectangle> void join(T other) {
+        float left   = Float.min(this.startLocation.get(0), other.startLocation.get(0));
+        float right  = Float.max(this.rightTopPoint.get(0), other.rightTopPoint.get(0));
+        float top    = Float.max(this.rightTopPoint.get(1), other.rightTopPoint.get(1));
+        float bottom = Float.min(this.startLocation.get(1), other.startLocation.get(1));
+
+        this.startLocation = new Vector(left, bottom, this.startLocation.get(2));
+        this.endLocation = new Vector(right, bottom, this.endLocation.get(2));
+        this.rightTopPoint = new Vector(right, top, this.rightTopPoint.get(2));
+    }
+
+    public <T extends Rectangle> boolean contains(T other) {
+        boolean left   = this.startLocation.get(0) <= other.startLocation.get(0);
+        boolean right  = this.rightTopPoint.get(0) >= other.rightTopPoint.get(0);
+        boolean top    = this.rightTopPoint.get(1) >= other.rightTopPoint.get(1);
+        boolean bottom = this.startLocation.get(1) <= other.startLocation.get(1);
+
+        return left && right && top && bottom;
+    }
 }
