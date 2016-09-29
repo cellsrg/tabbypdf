a00e3721741d3289450584b717258d68315fdb53
aaltaev <andalt92@gmail.com>
2016-09-29 14:04:33 +0800

    Added constructor and equals method
    Added constructor with line coordinate parameter and equals method

--- src/main/java/ru/icc/cells/common/Ruling.java
+++ src/main/java/ru/icc/cells/common/Ruling.java
@@ -4,11 +4,15 @@ import com.itextpdf.awt.geom.Point2D;
 
 public class Ruling {
     private final Point2D startLocation;
-    private final Point2D  endLocation;
+    private final Point2D endLocation;
 
     public Ruling(Point2D startLocation, Point2D endLocation) {
         this.startLocation = startLocation;
         this.endLocation = endLocation;
+    }
+
+    public Ruling(float x1, float y1, float x2, float y2) {
+        this(new Point2D.Float(x1, y1), new Point2D.Float(x2, y2));
     }
 
     public Point2D getStartLocation() {
@@ -17,5 +21,17 @@ public class Ruling {
 
     public Point2D getEndLocation() {
         return endLocation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Ruling ruling = (Ruling) o;
+
+        if (startLocation != null ? !startLocation.equals(ruling.startLocation) : ruling.startLocation != null)
+            return false;
+        return endLocation != null ? endLocation.equals(ruling.endLocation) : ruling.endLocation == null;
     }
 }
