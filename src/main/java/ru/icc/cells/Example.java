f90c4bb9df5c9acc4787c4269019046f3319187c
aaltaev <andalt92@gmail.com>
2016-10-10 16:38:31 +0800

    Example updated in order to TextChunkProcessorConfiguration

--- src/main/java/ru/icc/cells/Example.java
+++ src/main/java/ru/icc/cells/Example.java
@@ -3,11 +3,15 @@ package ru.icc.cells;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import ru.icc.cells.common.*;
 import ru.icc.cells.debug.visual.PdfBoxWriter;
-import ru.icc.cells.detectors.TableBoundingDetector;
+import ru.icc.cells.detectors.TableBoxDetector;
 import ru.icc.cells.detectors.TableRegionDetector;
 import ru.icc.cells.detectors.TextLineDetector;
-import ru.icc.cells.utils.TextChunkProcessor;
 import ru.icc.cells.utils.content.PdfContentExtractor;
+import ru.icc.cells.utils.processing.TextChunkProcessor;
+import ru.icc.cells.utils.processing.TextChunkProcessorConfiguration;
+import ru.icc.cells.utils.processing.filter.bi.*;
+import ru.icc.cells.utils.processing.filter.tri.CutInAfterTriFilter;
+import ru.icc.cells.utils.processing.filter.tri.CutInBeforeTriFilter;
 
 import java.awt.*;
 import java.io.File;
@@ -34,17 +38,37 @@ public class Example {
                 for (int pageNumber = 0; pageNumber < extractor.getNumberOfPages(); pageNumber++) {
                     Page page = extractor.getPageContent(pageNumber + 1);
                     writer.setPage(pageNumber);
-                    List<TextBlock>     textBlocks     = new TextChunkProcessor(page).process();
-                    List<TextLine>      textLines      = new TextLineDetector().detect(textBlocks);
-                    List<TableRegion>   tableRegions   = new TableRegionDetector().detect(textLines);
-                    List<TableBounding> tableBoundings = new TableBoundingDetector(textLines).detect(tableRegions);
+                    TextChunkProcessorConfiguration configuration = new TextChunkProcessorConfiguration()
+                            /*VERTICAL FILTERS*/
+                            .addFilter(new HorizontalPositionBiFilter())
+                            .addFilter(new SpaceWidthBiFilter())
+                            /*HORIZONTAL FILTERS*/
+                            .addFilter(new VerticalPositionBiFilter())
+                            .addFilter(new HeightBiFilter())
+                            .addFilter(new CutInAfterTriFilter())
+                            .addFilter(new CutInBeforeTriFilter())
+                            /*COMMON FILTERS*/
+                            .addFilter(new LinesBetweenChunksBiFilter(page.getRulings()))
+//                            .addFilter(new EqualFontFamilyBiFilter())
+                            .addFilter(new EqualFontSizeBiFilter());
 
-                    for (TableBounding tableBounding : tableBoundings) {
+
+                    List<TextBlock>   textBlocks   = new TextChunkProcessor(page, configuration).process();
+                    List<TextLine>    textLines    = new TextLineDetector().detect(textBlocks);
+                    List<TableRegion> tableRegions = new TableRegionDetector().detect(textLines);
+                    List<TableBox>    tableBoxes   = new TableBoxDetector(textLines).detect(tableRegions);
+
+//                    writer.setColor(Color.BLACK);
+//                    page.getOriginChunks().forEach(writer::drawRect);
+                    writer.setColor(Color.ORANGE);
+                    textBlocks.forEach(writer::drawRect);
+
+                    for (TableBox tableBox : tableBoxes) {
                         writer.setColor(Color.RED);
-                        writer.drawRect(tableBounding);
-                        for (TableRegion tableRegion : tableBounding.getTableRegions()) {
-                            writer.setColor(Color.CYAN);
-                            tableRegion.getGaps().forEach(writer::drawRect);
+                        writer.drawRect(tableBox);
+                        for (TableRegion tableRegion : tableBox.getTableRegions()) {
+/*                            writer.setColor(Color.CYAN);
+                            tableRegion.getGaps().forEach(writer::drawRect);*/
                             writer.setColor(Color.BLUE);
                             writer.drawRect(tableRegion);
                             for (TextLine textLine : tableRegion.getTextLines()) {
