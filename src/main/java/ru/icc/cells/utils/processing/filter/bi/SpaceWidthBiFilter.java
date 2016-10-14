1ecbf972a1c7974b4b93072c5424a64654691f2b
aaltaev <andalt92@gmail.com>
2016-10-14 17:16:48 +0800

    Space-dependent spaceWidth

--- src/main/java/ru/icc/cells/utils/processing/filter/bi/SpaceWidthBiFilter.java
+++ src/main/java/ru/icc/cells/utils/processing/filter/bi/SpaceWidthBiFilter.java
@@ -19,9 +19,20 @@ public class SpaceWidthBiFilter extends BiHeuristic<TextChunk> {
     @Override
     public boolean test(TextChunk first, TextChunk second) {
         if (first.getRight() >= second.getLeft()) return true;
+        float spaceWidth;
+        if (first.getText().contains(" ")) {
+            spaceWidth = first.getCharSpaceWidth();
+        } else {
+            spaceWidth = new Chunk(' ', new Font(first.getChunkFont())).getWidthPoint();
+            if (spaceWidth == 0) {
+                spaceWidth = 3;
+            }
+        }
         /* Creating a chunk with only " " content and taking its width */
-        //        float spaceWidth    = leftChunk.getCharSpaceWidth();
-        float spaceWidth    = new Chunk(' ', new Font(first.getChunkFont())).getWidthPoint();
+        //        System.out.println();
+        //        System.out.println(spaceWidth);
+        //        /*float */spaceWidth    = new Chunk(' ', new Font(first.getChunkFont())).getWidthPoint();
+        //        System.out.println(spaceWidth);
         float chunkDistance = second.getLeft() - first.getRight();
         return chunkDistance <= spaceWidth * spaceWidthMultiplier;
     }
