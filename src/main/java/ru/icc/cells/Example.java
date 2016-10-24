2ce202a479ea1c5ce138481d6095cda5f8ba90db
aaltaev <andalt92@gmail.com>
2016-10-24 11:58:01 +0800

    Example updated

--- src/main/java/ru/icc/cells/Example.java
+++ src/main/java/ru/icc/cells/Example.java
@@ -1,10 +1,8 @@
 package ru.icc.cells;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
 import ru.icc.cells.common.Page;
 import ru.icc.cells.common.TableBox;
 import ru.icc.cells.common.TextBlock;
-import ru.icc.cells.debug.visual.PdfBoxWriter;
 import ru.icc.cells.detectors.TableDetector;
 import ru.icc.cells.utils.content.PdfContentExtractor;
 import ru.icc.cells.utils.processing.TextChunkProcessor;
@@ -13,28 +11,33 @@ import ru.icc.cells.utils.processing.filter.Heuristic;
 import ru.icc.cells.utils.processing.filter.bi.*;
 import ru.icc.cells.utils.processing.filter.tri.CutInAfterTriHeuristic;
 import ru.icc.cells.utils.processing.filter.tri.CutInBeforeTriHeuristic;
+import ru.icc.cells.writers.TableBoxToXmlWriter;
 
-import java.awt.*;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Example {
     public static final String TEST_PDF_DIR = "src/test/resources/pdf/";
     public static final String SAVE_PDF_DIR = "src/test/resources/pdf/edit/";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, TransformerException, ParserConfigurationException {
         File folder = new File(TEST_PDF_DIR);
         for (File file : folder.listFiles(File::isFile)) {
-            processFile(file);
+            TableBoxToXmlWriter writer = new TableBoxToXmlWriter();
+            writer.write(findTables(file), file.getName(),
+                         SAVE_PDF_DIR + "xml/" + file.getName().substring(0, file.getName().lastIndexOf('.')) +
+                         "-reg-output.xml");
         }
-        //processFile(new File("src/test/resources/pdf/Aeroflot_FS_2006.10.pdf"));
+        //findTables(new File("src/test/resources/pdf/Aeroflot_FS_2006.10.pdf"));
     }
 
     private static TextChunkProcessorConfiguration getConfiguration() {
         return new TextChunkProcessorConfiguration()
-                            /*VERTICAL FILTERS*/
-                            .addFilter(new HorizontalPositionBiHeuristic())
+                            /*VERTICAL FILTERS*/.addFilter(new HorizontalPositionBiHeuristic())
                             .addFilter(new SpaceWidthBiFilter().enableListCheck(true))
                             /*HORIZONTAL FILTERS*/
                             .addFilter(new VerticalPositionBiHeuristic())
@@ -49,42 +52,26 @@ public class Example {
                             .addStringsToReplace(new String[]{"•", "", " ", "_", "\u0002"/**/});
     }
 
-    private static void processFile(File file) {
+    private static List<TableBox> findTables(File file) {
         System.out.println(file.getName());
-        String path = file.getAbsolutePath();
+        List<TableBox> tableBoxes = new ArrayList<>();
         try {
-            PdfContentExtractor extractor = new PdfContentExtractor(path);
-            PDDocument          document  = PDDocument.load(new File(path));
-            PdfBoxWriter        writer    = new PdfBoxWriter(document);
-
-
+            PdfContentExtractor extractor = new PdfContentExtractor(file.getAbsolutePath());
             for (int pageNumber = 0; pageNumber < extractor.getNumberOfPages(); pageNumber++) {
-                Page page = extractor.getPageContent(pageNumber + 1);
-                writer.setPage(pageNumber);
+                Page page = extractor.getPageContent(pageNumber);
                 TextChunkProcessorConfiguration configuration =
                         getConfiguration().addFilter(new LinesBetweenChunksBiHeuristic(page.getRulings()));
-                List<TextBlock> textBlocks = new TextChunkProcessor(page, configuration).process();
-                TableDetector  tableDetector = new TableDetector(null);
-                List<TableBox> tableBoxes    = tableDetector.detect(textBlocks);
-
-                writer.setColor(Color.PINK);
-                writer.drawRects(page.getOriginChunks());
-
-                writer.setColor(Color.ORANGE);
-                writer.drawRects(textBlocks);
-
-                writer.setColor(Color.RED);
-                writer.drawRects(tableBoxes);
-                for (TableBox tableBox : tableBoxes) {
-                    writer.setColor(Color.BLUE);
-                    writer.drawRects(tableBox.getTableRegions());
+                List<TextBlock> textBlocks     = new TextChunkProcessor(page, configuration).process();
+                TableDetector   tableDetector  = new TableDetector(null);
+                List<TableBox>  pageTableBoxes = tableDetector.detect(textBlocks);
+                for (TableBox tableBox : pageTableBoxes) {
+                    tableBox.setPageNumber(pageNumber + 1);
                 }
+                tableBoxes.addAll(pageTableBoxes);
             }
-            writer.close();
-            document.save(SAVE_PDF_DIR + file.getName());
-            document.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return tableBoxes;
     }
 }
