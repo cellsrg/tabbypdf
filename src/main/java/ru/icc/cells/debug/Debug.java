5410cebe8576d25a14f11ed393dae17b240f483d
aaltaev <andalt92@gmail.com>
2016-10-24 16:42:34 +0800

    Debug tool

---
+++ src/main/java/ru/icc/cells/debug/Debug.java
@@ -0,0 +1,93 @@
+package ru.icc.cells.debug;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import ru.icc.cells.common.Rectangle;
+import ru.icc.cells.common.Ruling;
+import ru.icc.cells.debug.visual.PdfBoxWriter;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class Debug {
+
+    private static PDDocument   doc;
+    private static PdfBoxWriter writer;
+    public static final boolean ENABLE_DEBUG = false;
+
+    private Debug() {}
+
+    public static void handleFile(File file) throws IOException {
+        if (ENABLE_DEBUG) {
+            doc = PDDocument.load(file);
+            writer = new PdfBoxWriter(doc);
+        }
+    }
+
+    public static void setColor(Color color) {
+        if (ENABLE_DEBUG) {
+            if (writer != null) {
+                writer.setColor(color);
+            }
+        }
+    }
+
+    public static void printText(String text, float x, float y) {
+        if (ENABLE_DEBUG) {
+            if (writer != null) {
+                writer.printText(text, x, y);
+            }
+        }
+    }
+
+    public static <T extends Rectangle> void drawRect(T rect) {
+        if (ENABLE_DEBUG) {
+            if (writer != null) {
+                writer.drawRect(rect);
+            }
+        }
+    }
+
+    public static <T extends Rectangle> void drawRects(List<T> rects) {
+        if (ENABLE_DEBUG) {
+            if (writer != null) {
+                writer.drawRects(rects);
+            }
+        }
+    }
+
+    public static void drawRuling(Ruling ruling) {
+        if (ENABLE_DEBUG) {
+            if (writer != null) {
+                writer.drawRuling(ruling);
+            }
+        }
+    }
+
+    public static void close(String savePath) throws IOException {
+        if (ENABLE_DEBUG) {
+            if (writer != null) {
+                writer.close();
+                doc.save(savePath);
+                doc.close();
+            }
+        }
+    }
+
+    public static void print(String s) {
+        if (ENABLE_DEBUG) {
+            System.out.print(s);
+        }
+    }
+
+    public static void println(String s) {
+        if (ENABLE_DEBUG) {
+            System.out.println(s);
+        }
+    }
+
+    public static void println() {
+        println("");
+    }
+}
